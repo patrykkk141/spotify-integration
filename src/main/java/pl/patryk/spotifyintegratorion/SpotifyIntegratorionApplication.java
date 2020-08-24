16b6609debe5b61a9bcc6f4c3b22f9a0f5c121c7
Patryk.Iwanczyk <patryk.iwanczyk0102@gmail.com>
2020-08-24 15:05:22 +0200

    Initial commit

---
+++ src/main/java/pl/patryk/spotifyintegratorion/SpotifyIntegratorionApplication.java
@@ -0,0 +1,13 @@
+package pl.patryk.spotifyintegratorion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyIntegratorionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpotifyIntegratorionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/patryk/spotifyintegratorion/SpotifyIntegratorionApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.patryk.spotifyintegratorion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyIntegratorionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
