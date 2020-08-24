645a84e5eb1127e47027f38558a664642b61f94b
Patryk.Iwanczyk <patryk.iwanczyk0102@gmail.com>
2020-08-24 15:10:25 +0200

    Fix type in integration word

--- src/main/java/pl/patryk/spotifyintegration/SpotifyIntegrationApplication.java
+++ src/main/java/pl/patryk/spotifyintegration/SpotifyIntegrationApplication.java
@@ -1,13 +1,13 @@
-package pl.patryk.spotifyintegratorion;
+package pl.patryk.spotifyintegration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpotifyIntegratorionApplication {
+public class SpotifyIntegrationApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpotifyIntegratorionApplication.class, args);
+        SpringApplication.run(SpotifyIntegrationApplication.class, args);
     }
 
 }
